ea446f971f70a0d210320cb4ade6ba5aad76b112
somilyadav7 <mesomilyadav7@gmail.com>
2023-09-17 13:42:39 +0530

    Final

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 //const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
-
+const PORT = process.env.PORT || 3000;
 const app = express();
-
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://sewy999:sewy999@cluster1.8hu0bkv.mongodb.net/ToDoListDB",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://sewy999:sewy999@cluster1.8hu0bkv.mongodb.net/ToDoList",{useNewUrlParser:true});
 
 const itemSchema = {
   name: String
@@ -136,6 +135,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("Server started on port 3000");
 });
